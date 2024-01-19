23c99c8fb0aa8f62a412f6290462e6c76d28e14f
David Andrs <andrsd@gmail.com>
2024-01-19 16:04:46 -0700

    cl4ng-f0rm4t

--- src/view.cpp
+++ src/view.cpp
@@ -2,31 +2,30 @@
 #include "mainwindow.h"
 #include <QOpenGLFunctions>
 
-View::View(MainWindow * main_wnd) :
-    main_window(main_wnd)
-{
-
-}
+View::View(MainWindow * main_wnd) : main_window(main_wnd) {}
 
 View::~View() {}
 
-void View::initializeGL()
+void
+View::initializeGL()
 {
     // Set up the rendering context, load shaders and other resources, etc.:
-    QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
+    QOpenGLFunctions * f = QOpenGLContext::currentContext()->functions();
     f->glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 }
 
-void View::resizeGL(int w, int h)
+void
+View::resizeGL(int w, int h)
 {
     // Update projection matrix and other size related settings:
     this->projection.setToIdentity();
     this->projection.perspective(45.0f, w / float(h), 0.01f, 100.0f);
 }
 
-void View::paintGL()
+void
+View::paintGL()
 {
     // Draw the scene:
-    QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
+    QOpenGLFunctions * f = QOpenGLContext::currentContext()->functions();
     f->glClear(GL_COLOR_BUFFER_BIT);
 }
